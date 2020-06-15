c7c575bc187412f5307add5c99d0d0265c420e21
Jessie Li <jessie@jessies-mbp.lan>
2020-06-15 16:18:02 -0700

    add pages for equations

--- generate_exercise.py
+++ generate_exercise.py
@@ -1,32 +1,58 @@
+import cv2
+import numpy as np
 import random
 from datetime import datetime
 random.seed(datetime.now())
-
-numbers = 76  # number of equations to generate.
-first_op_range = (0, 10)  # first operands range in [0, 10]
-second_op_range = (0, 10)  # second operands range in [0, 100]
-operators = ['+', '-']  # set which operator to use, supports any  ['+', '-', 'x']
+import argparse
 
 
-numbers = numbers // 2 * 2  # Make it even so each line can contain two equations.
+def generate_one_page(page_name, first_max, second_max):
+  numbers = 36  # number of equations to generate.
+  first_op_range = (0, first_max)  # first operands range in [0, 10]
+  second_op_range = (0, second_max)  # second operands range in [0, 100]
+  operators = ['+', '-']  # set which operator to use, supports any  ['+', '-', 'x']
+  numbers = numbers // 2 * 2  # Make it even so each line can contain two equations.
+  rand_num = numbers * 2
 
-rand_num = numbers * 2
+  height = 1280
+  width = 960
+  line_per_page = 20
+  line_height = height // line_per_page
+  im = np.ones((height, width, 3), np.uint8) * 255
+  font = cv2.FONT_HERSHEY_SIMPLEX 
+  fontScale = 1
+  color = (0, 0, 0)   
+  thickness = 2
+
+  dx = 50
+  dy = 100
+  for i in range(numbers):
+    a = random.randint(*first_op_range)
+    b = random.randint(*second_op_range)
+    sign = random.choice(operators)
+    if sign == '-':
+      if a < b:
+        a, b = b, a
+    if i % 2 == 0:
+      x = dx
+    else:
+      x = dx + width // 2
+    y = dy + (i//2) * line_height
+    ops = '{:<5d}{}{:5d}  =     '.format(a, sign, b) 
+    im = cv2.putText(im, ops,(x,y), font, fontScale, color, thickness, cv2.LINE_AA)
+    print(ops)
+  cv2.imwrite(page_name + '.png', im)
 
 
-generate_nums = []
-file = open("numbers.txt", 'w')
-for i in range(numbers):
-  a = random.randint(*first_op_range)
-  b = random.randint(*second_op_range)
-  sign = random.choice(operators)
-  if sign == '-':
-    if a < b:
-      a, b = b, a
-  str_val = '{}\t{}\t{}\t=\t\t\t'.format(a, sign, b)
-  if i % 2 == 0:
-    str_val += '\t'
-  else:
-    str_val += '\n'
-  print(str_val)
-  file.write(str_val)
-file.close()
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--pages', type=int, default=1)
+  parser.add_argument('--first_max', type=int, default=20)
+  parser.add_argument('--second_max', type=int, default=20)
+  args = parser.parse_args()
+  for page in range(args.pages):
+    generate_one_page('numbers{:02d}'.format(page), args.first_max, args.second_max)
+
+
+if __name__ == '__main__':
+  main()
--- readme.md
+++ readme.md
@@ -1,11 +1,11 @@
 ## Math exercise generator
 
-The python program generates some arithmetic exercise for kid, supported operators including '+', '-', 'x', and you can specify the range of operands. 
+The python program generates some arithmetic exercise for kid, supported operators including '+', '-' etc. and you can specify the range of operands. 
 
 ## Usage
   * [optional] Change the parametors of generate_exercise.py if you want to change the range of operands or type of operators. 
-  * run program by running ```python generate_exercise.py```
-  * copy the numbers saved in numbers.txt to numbers.docx and print it.
+  * run program by running ```python generate_exercise.py --pages=1 --first_max=10 --second_max=10```
+  * each page has 36 equations.
 
 ## Generated exercise example.
-![Image](example.PNG)
+![Image](example.png)
